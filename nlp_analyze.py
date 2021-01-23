924c0cac416eb1cfcf440821e7fda7c965f1fcb9
Mark Liu <markliu202@gmail.com>
2021-01-23 17:02:22 -0500

    cleanup

--- nlp_analyze.py
+++ nlp_analyze.py
@@ -5,10 +5,8 @@ from google.cloud import language_v1
 # testing_text = u"I am so happy! This is a very happy sounding text!! :)"
 
 def get_sentiment_score(text):
-  # Instantiates a client
   client = language_v1.LanguageServiceClient.from_service_account_json("./cred.json")
   document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-  # Detects the sentiment of the text
   sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
   return sentiment.score
 
@@ -16,14 +14,12 @@ def get_sentiment_score(text):
 def get_sentiment_magnitude(text):
   client = language_v1.LanguageServiceClient.from_service_account_json("./cred.json")
   document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-  # Detects the sentiment of the text
   sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
   return sentiment.magnitude
   
 
 def get_entity_link(text):
   client = language_v1.LanguageServiceClient.from_service_account_json("./cred.json")
-  # Available types are PLAN_TEXT and HTML
   type_ = language_v1.Document.Type.PLAIN_TEXT
   language = "en"
   document = {"content": text, "type_": type_, "language": language}
@@ -39,4 +35,4 @@ def get_entity_link(text):
   return link
 
 # print(get_entity_link("Google, headquartered in Mountain View (1600 Amphitheatre Pkwy, Mountain View, CA 940430), unveiled the new Android phone for $799 at the Consumer Electronic Show. Sundar Pichai said in his keynote that users love their new Android phones."))
-print(get_entity_link("My iPhone just dided"))
+# print(get_entity_link("My iPhone just dided"))
