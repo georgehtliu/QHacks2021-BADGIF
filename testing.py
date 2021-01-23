ec6940abd431f4ca96b67fc5d0c5ed04b67d8270
SmilestheSad <vzheng@gmail.com>
2021-01-23 00:04:15 -0500

    basic chatbot gif stuff

--- testing.py
+++ testing.py
@@ -1,12 +1,13 @@
 import discord
-
+import requests
+import json
 # settings.py
 from dotenv import load_dotenv
 load_dotenv()
 
 import os 
 SECRET_KEY = os.getenv("token")
-
+TENOR_KEY = os.getenv("tenorkey")
 client = discord.Client()
 
 @client.event
@@ -19,7 +20,19 @@ async def on_message(message):
         return
 
     if message.content.startswith('$gif'):
-        vals = message.content.split("")
-        await message.channel.send(vals)
+        vals = message.content.split(" ")[1:]
+        if vals[0] == "dog":
+            search_term = "LOL"
+        elif vals[0] == "im":
+            search_term = "bye"
+        else: 
+            search_term = vals[0]
+        r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=1" % (search_term, TENOR_KEY))
+        parsed = json.loads(r.content)
+        good = parsed['results'][0]['media'][0]['tinygif']['url']
+        await message.channel.send(good)
+        print(parsed['weburl'])
+    
 
-client.run(SECRET_KEY)
+
+client.run("""ODAyMzgxNDU0MTgwMjIwOTg4.YAuZ9g.-sk5ynRGuIaKn06bPy4u2cjpEYk""")
