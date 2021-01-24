56da34fba038db7cf7458750f7baa23bb8f5a42f
George Liu <georgeliu220@gmail.com>
2021-01-23 22:32:32 -0500

    fix schema

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,16 @@
 import express from "express";
 
-import { getPosts, createTrans, deleteAllPosts } from "../controllers/posts.js";
+import {
+  getPosts,
+  createTrans,
+  deleteAllPosts,
+  getUserPosts,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.get("/:id", getUserPosts);
 router.post("/", createTrans);
 router.delete("/all", deleteAllPosts);
 
