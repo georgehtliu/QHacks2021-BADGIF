f8b1336eb0e577c727f866cb345c7b6dab5638c3
Anson He <ansonjwhe@gmail.com>
2021-01-24 00:23:35 -0500

    added colour to gifcards

--- client/src/Components/GIFCard.js
+++ client/src/Components/GIFCard.js
@@ -4,13 +4,29 @@ import 'antd/dist/antd.css';
 import React from 'react'
 
 export default function GIFCard(props) {
-    const moodColor = props.mood <= 0 ? '#86C67C' : '#ff9999'
+
+    const hslToHex = (h, s, l) => {
+        l /= 100;
+        const a = s * Math.min(l, 1 - l) / 100;
+        const f = n => {
+          const k = (n + h / 30) % 12;
+          const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+          return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+        };
+        return `#${f(0)}${f(8)}${f(4)}`;
+    }
+
+    
+    const moodColor = (moodvalue) => {
+        var hue=(((1-moodvalue)/2)*120).toString(10)
+        return hslToHex(hue, 60, 50)
+    }
 
     return (
         <>
             <Card title = {props.user}
-            headStyle={{backgroundColor:moodColor}}
-            bodyStyle={{backgroundColor:moodColor}}>
+            headStyle={{backgroundColor:moodColor(props.mood)}}
+            bodyStyle={{backgroundColor:moodColor(props.mood)}}>
                 Mood: {props.mood}
                 Message: {props.message}
             </Card>
